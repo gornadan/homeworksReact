fbc6ae817a61858c675d931303027506a29882e2
Natalia Danilchanka <nataladanilcenko@gmail.com>
2021-01-12 12:49:17 +0300

    homwork6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import SuperEditableSpan from "./common/c4-SuperEditableSpan/SuperEditableSpan";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 import {restoreState, saveState} from "./localStorage/localStorage";
+import s from "./HW6.module.css"
 
 function HW6() {
     const [value, setValue] = useState<string>("");
@@ -10,13 +11,13 @@ function HW6() {
         saveState<string>("editable-span-value", value);
     };
     const restore = () => {
-        // setValue();
+         setValue(restoreState("editable-span-value", value));
     };
 
     return (
-        <div>
+        <div className={s.wrapper}>
             <hr/>
-            homeworks 6
+            <h2>homeworks 6</h2>
 
             {/*should work (должно работать)*/}
             <div>
@@ -26,9 +27,10 @@ function HW6() {
                     spanProps={{children: value ? undefined : "enter text..."}}
                 />
             </div>
+            <div className={s.btm}>
             <SuperButton onClick={save}>save</SuperButton>
             <SuperButton onClick={restore}>restore</SuperButton>
-
+            </div>
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperEditableSpan/>*/}
