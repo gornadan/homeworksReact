6395eef1be4cc5e46f32129380d120e754ee32d3
Natalia Danilchanka <nataladanilcenko@gmail.com>
2021-01-12 11:21:26 +0300

    correction HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -4,30 +4,22 @@ import s from './Header.module.css'
 import {NavLink} from "react-router-dom";
 
 
-
-
-
 function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(true );
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen)
     };
     return (
-        <div className={isMenuOpen ? s.item1 : s.item1_active } >
-            {/*//isMenuOpen ?: s.item1_active*/}
-
-            <span className={s.menu_btm} onClick={toggleMenu}>x</span>
-            <div className={s.item}>
-                <NavLink to={PATH.PRE_JUNIOR}>PRE_JUNIOR</NavLink>
+        <div className={s.wrapper}>
+            <div className={s.menu_btm} onClick={toggleMenu}>&#9776;</div>
+            <div className={isMenuOpen ? s.item1 : s.item1_active}>
+               <div><NavLink to={PATH.PRE_JUNIOR} className={s.item}>PRE_JUNIOR</NavLink></div>
+                <div><NavLink to={PATH.JUNIOR}  className={s.item}>JUNIOR</NavLink></div>
+                <div><NavLink to={PATH.MIDDLE}  className={s.item}>MIDDLE</NavLink></div>
             </div>
-            <div className={s.item}>
-                <NavLink to={PATH.JUNIOR}>JUNIOR</NavLink></div>
-            <div className={s.item}>
-                <NavLink to={PATH.MIDDLE}>MIDDLE</NavLink></div>
-
-
         </div>
+
     );
 }
 
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className="wrapper">
+            <div className="wrap_box">
+                <div className="error">404</div>
+                <div className="error_2">Page not found!</div>
+                <div className="error_3">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in
+                    the first place?<p>Let's go <a href="#">home.</a> </p></div>
+            </div>
         </div>
     );
 }
